ff4faa92b972a0d5c23bfa7699ab6ad33c6b61c6
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2022-12-11 14:24:38 +0530

    Joint Effect
    reduce stress after using joint, which doesn't work at the moment

--- client/Emote.lua
+++ client/Emote.lua
@@ -665,6 +665,39 @@ function OnEmotePlay(EmoteName, textureVariation)
     end
 end
 
+RegisterNetEvent('animations:client:SmokeWeed')
+AddEventHandler('animations:client:SmokeWeed', function()
+  SmokingWeed = true
+  Citizen.CreateThread(function()
+    while SmokingWeed do
+      Citizen.Wait(10000)
+      TriggerServerEvent('hud:server:RelieveStress', math.random(15, 18))
+      RelieveCount = RelieveCount + 1
+      if RelieveCount == 6 then
+        if ChosenDict == "MaleScenario" and IsInAnimation then
+          ClearPedTasksImmediately(PlayerPedId())
+          IsInAnimation = false
+          DebugPrint("Forced scenario exit")
+        elseif ChosenDict == "Scenario" and IsInAnimation then
+          ClearPedTasksImmediately(PlayerPedId())
+          IsInAnimation = false
+          DebugPrint("Forced scenario exit")
+        end
+
+        if IsInAnimation then
+          ClearPedTasks(PlayerPedId())
+          DestroyAllProps()
+          IsInAnimation = false
+        end
+
+        if SmokingWeed then
+          SmokingWeed = false
+          RelieveCount = 0
+        end
+      end
+    end
+  end)
+end)
 -----------------------------------------------------------------------------------------------------
 ------ Some exports to make the script more standalone! (by Clem76) ---------------------------------
 -----------------------------------------------------------------------------------------------------
